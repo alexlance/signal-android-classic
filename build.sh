3f8a9ff50a485ad688e8c94002efb81597dca117
ubergeek77 <ubergeek77@users.noreply.github.com>
2023-04-01 23:07:19 -0500

    Update for 6.15.3

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 set -euxo pipefail
 
 # Which versions of Signal to build APKs for
-versions="v6.5.0 v6.5.1 v6.5.2 v6.5.3 v6.5.4 v6.5.5 v6.5.6 v6.6.0 v6.6.1 v6.6.2 v6.6.3 v6.7.0 v6.7.1 v6.7.2 v6.7.3 v6.7.4 v6.7.5 v6.7.6 v6.8.0 v6.8.1 v6.8.2 v6.8.3 v6.9.0 v6.9.1 v6.9.2 v6.10.0 v6.10.1 v6.10.2 v6.10.3 v6.10.4 v6.10.5 v6.10.6 v6.10.7 v6.10.8 v6.10.9 v6.11.0 v6.11.1 v6.11.2 v6.11.3 v6.11.4 v6.11.5 v6.11.6 v6.11.7 v6.12.0 v6.12.1 v6.12.2 v6.12.3 v6.12.4 v6.12.5 v6.12.6 v6.13.0 v6.13.1 v6.13.2 v6.13.3 v6.13.4 v6.13.5 v6.13.6 v6.13.7 v6.13.8 v6.14.0 v6.14.1 v6.14.2 v6.14.3 v6.14.4"
+versions="v6.5.0 v6.5.1 v6.5.2 v6.5.3 v6.5.4 v6.5.5 v6.5.6 v6.6.0 v6.6.1 v6.6.2 v6.6.3 v6.7.0 v6.7.1 v6.7.2 v6.7.3 v6.7.4 v6.7.5 v6.7.6 v6.8.0 v6.8.1 v6.8.2 v6.8.3 v6.9.0 v6.9.1 v6.9.2 v6.10.0 v6.10.1 v6.10.2 v6.10.3 v6.10.4 v6.10.5 v6.10.6 v6.10.7 v6.10.8 v6.10.9 v6.11.0 v6.11.1 v6.11.2 v6.11.3 v6.11.4 v6.11.5 v6.11.6 v6.11.7 v6.12.0 v6.12.1 v6.12.2 v6.12.3 v6.12.4 v6.12.5 v6.12.6 v6.13.0 v6.13.1 v6.13.2 v6.13.3 v6.13.4 v6.13.5 v6.13.6 v6.13.7 v6.13.8 v6.14.0 v6.14.1 v6.14.2 v6.14.3 v6.14.4 v6.15.3"
 
 # Check that we have some of the tools installed
 which keytool
@@ -35,7 +35,9 @@ for SIGNAL_TAG in $versions; do
   # different versions need slightly different patches
   v=$(echo $SIGNAL_TAG | tr -d '.v')
 
-  if [ "$v" -gt 6139 ]; then
+  if [ "$v" -gt 6152 ]; then
+    patch="patch-001-forced-upgrades-6.14.4.diff patch-002-enable-sms-6.15.3.diff"
+  elif [ "$v" -gt 6139 ]; then
     patch="patch-001-forced-upgrades-6.14.4.diff patch-002-enable-sms-6.13.8.diff"
   elif [ "$v" -gt 6136 ]; then
     patch="patch-001-forced-upgrades-6.9.0.diff patch-002-enable-sms-6.13.8.diff"
